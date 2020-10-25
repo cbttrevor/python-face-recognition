d2721475ceac451eaefb3030f3fd6faa00b5a3af
Trevor Sullivan <trevor@trevorsullivan.net>
2020-10-25 16:48:57 -0600

    Add Python script to find faces

--- 04-Find Faces in Images/01-find-faces.py
+++ 04-Find Faces in Images/01-find-faces.py
@@ -1,5 +1,29 @@
-import os
+from os.path import realpath, dirname
+import glob
+import face_recognition
+import numpy
+import cv2
+from functools import reduce
 
-script_dir = os.path.realpath(os.path.dirname(__file__))
+# Find the script's current directory
+script_dir = realpath(dirname(__file__))
+assets_dir = '{0}/assets'.format(dirname(script_dir))
 
-os.
+# Find all files with .jpg file extension in the ../assets/ directory
+file_list = glob.glob('{0}/*.jpg'.format(assets_dir))
+
+# Print out the list of files
+print(file_list)
+
+for file in file_list:
+    img = face_recognition.load_image_file(file)
+
+    # Learning point: face locations are returned as arrays
+    locations = face_recognition.face_locations(img)
+    print(locations)
+    for face_rect in locations:
+        print(face_rect)
+        cv2.rectangle(img, (face_rect[3], face_rect[0]), (face_rect[1], face_rect[2]), (0,0,255))
+
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    cv2.imwrite('{0}-output.jpg'.format(file), img)
--- 04-Find Faces in Images/README.md
+++ 04-Find Faces in Images/README.md
@@ -1,3 +1,5 @@
 ## Learning Objective
 
 Now that we've downloaded some sample images, let's find faces in those images.
+The API for the `face-recognition` module is pretty straightforward.
+We can also use the `opencv-python` package to draw boxes around the faces, once we know their coordinates.
